ae5c4f6bd39ac9db9772dc9213423e21ac2cec81
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-09 19:34:54 +0100

    Updated config files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description='PyMassSpec interface to Agilent .d datafiles',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
