be499fb1fcd6e719c6cf83f66d1c20bebf00823a
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-02 16:09:02 +0000

    Updated config files.

--- README.rst
+++ README.rst
@@ -17,7 +17,7 @@ pyms-agilent
 	* - Docs
 	  - |docs| |docs_check|
 	* - Tests
-	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor|
+	  - |travis| |actions_windows| |actions_macos| |coveralls| |codefactor| |pre_commit_ci|
 	* - PyPI
 	  - |pypi-version| |supported-versions| |supported-implementations| |wheel|
 	* - Activity
@@ -94,6 +94,10 @@ pyms-agilent
 .. |pre_commit| image:: https://img.shields.io/badge/pre--commit-enabled-brightgreen?logo=pre-commit&logoColor=white
 	:target: https://github.com/pre-commit/pre-commit
 	:alt: pre-commit
+
+.. |pre_commit_ci| image:: https://results.pre-commit.ci/badge/github/domdfcoding/pyms-agilent/master.svg
+	:target: https://results.pre-commit.ci/latest/github/domdfcoding/pyms-agilent/master
+	:alt: pre-commit.ci status
 
 .. end shields
 
--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,22 +17,9 @@ import pathlib
 __all__ = [
 		"__copyright__",
 		"__version__",
-		"modname",
-		"pypi_name",
-		"__license__",
-		"__author__",
-		"short_desc",
-		"author",
-		"author_email",
-		"github_username",
-		"web",
-		"github_url",
 		"repo_root",
 		"install_requires",
 		"extras_require",
-		"project_urls",
-
-		"import_name",
 		]
 
 __copyright__ = """
@@ -40,23 +27,7 @@ __copyright__ = """
 """
 
 __version__ = "0.0.1"
-modname = "pyms-agilent"
-pypi_name = "pyms-agilent"
-import_name = "pyms_agilent"
-__license__ = "GNU Lesser General Public License v3 or later (LGPLv3+)"
-short_desc = 'PyMassSpec interface to Agilent .d datafiles'
-__author__ = author = 'Dominic Davis-Foster'
-author_email = 'dominic@davis-foster.co.uk'
-github_username = "domdfcoding"
-web = github_url = "https://github.com/domdfcoding/pyms-agilent"
+
 repo_root = pathlib.Path(__file__).parent
 install_requires = (repo_root / "requirements.txt").read_text(encoding="utf-8").split('\n')
 extras_require = {'all': []}
-
-
-
-project_urls = {
-		"Documentation": "https://pyms-agilent.readthedocs.io",
-		"Issue Tracker": f"{github_url}/issues",
-		"Source Code": github_url,
-		}
